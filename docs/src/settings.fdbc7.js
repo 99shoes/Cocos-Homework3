4e56e85b0b4b74229fd60e32432f294770e415cb
吳承翰 <hank413041@gmail.com>
2021-07-23 18:28:33 +0800

    add docs

---
+++ docs/src/settings.fdbc7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"a2c40"}};
